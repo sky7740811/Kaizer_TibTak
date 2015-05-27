774f2731d16431b89ada0cc276d7b79393a0eae6
Chihoon <Chihoon@LCH>
2015-05-27 20:35:11 +0200

    menu admin modif

--- projet/menu.php
+++ projet/menu.php
@@ -14,6 +14,7 @@ function menu() {
 function menu_admin() {
     echo '<ul>
         <li><a href="ListeCompte.php">Visualisation de la liste des comptes</a></li>
+        <li><a href="ListeTrajet.php">Visualisation de la liste des trajets</a></li>
         <li><a href="Modification.php">Caractériser son compte</a></li>
         <li><a href="infovehicule.php">Information sur le véhicule</a></li>
         <li><a href="AjoutTrajet.php">Ajout d\'un nouveau trajet</a></li>
